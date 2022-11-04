96766acc15825a5cac597d25763e00b217676af4
ThuyLinh110 <thuylinh1102001@gmail.com>
2022-11-04 13:25:39 +0700

    Create class LoginPage

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LoginPage;
+
+public class Main {
+    public static WebDriver driver ;
+    public static void setup ()
+    {
+        System.setProperty("webdriver.chrome.driver", "D:\\NTLINH\\AUTO\\Driver\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public static void main(String[] args)
+    {
+        setup();
+        LoginPage loginPage = new LoginPage();
+        loginPage.openPage("http://www.railwayb2.somee.com/Account/Login.cshtml", driver);
+        loginPage.login("linh@gmail.com","123456789");
+    }
+}
---
+++ src/pages/LoginPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+    //Locators
+
+
+    private WebDriver driver ;
+    private By txtUserName = By.xpath("//input[@id='username']");
+    private By txtPassword = By.xpath("//input[@id='password']");
+    private By btnLogin = By.xpath("//input[@value='Login']");
+
+    //WebElements
+    private WebElement getTxtUserName() {return driver.findElement(txtUserName) ;}
+    private WebElement getTxtPassword() {return driver.findElement(txtPassword) ;}
+    private WebElement getBtnLogin() {return driver.findElement(btnLogin) ;}
+
+
+    //Methods
+    public void openPage(String baseUrl, WebDriver driver)
+    {
+        driver.get(baseUrl);
+        this.driver = driver ;
+    }
+
+    public void login(String userName, String password)
+    {
+        getTxtUserName().sendKeys(userName);
+        getTxtPassword().sendKeys(password);
+        getBtnLogin().click();
+    }
+
+}
